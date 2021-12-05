bccac0907969b42b89c329eaecd03ef018770043
Marx-San <95501057+Marx-San@users.noreply.github.com>
2021-12-05 15:25:54 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View ,SafeAreaView, Alert} from 'react-native';
+import Navbar from './Navbar'
+
+export default function App() {
+ 
+
+
+ 
+
+
+
+
+  return (
+  <Navbar  />
+ 
+ 
+    
+  );
+}
+
+
+
